42570258c32b0a593113d029b6559c82ddc8c5d5
Dileep-s-s <dileep2005s@gmail.com>
2024-04-03 20:45:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const toggleBtn = document.getElementById('toggleBtn');
+const toggleBtndrop = document.querySelector('.toggle-btn i');
+const dropdown = document.querySelector('.dropdown_menu');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('.heads');
+const curzordot = document.querySelector('[data-curzor-dot]');
+const curzoroutline = document.querySelector('[data-curzor-outline]');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.anchors  [href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+}
+
+
+toggleBtn.onclick = function () {
+    dropdown.classList.toggle('open');
+    const isOpen = dropdown.classList.contains('open');
+    toggleBtndrop.className = isOpen ? 'fas fa-times' : 'fas fa-bars';
+};
+
